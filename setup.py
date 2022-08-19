ab85db86bbf04607511d2dbd55353fb3697e2dc7
George Rey <1606347+jerzydziewierz@users.noreply.github.com>
2022-08-19 20:34:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
     name='mict',  # it seems that doing that in setup.cfg doesn't work for me
-    version='0.0.35',
+    version='0.0.37',
 )
 
 
@@ -10,4 +10,4 @@ def get_version():
     try using `git rev-parse HEAD --short` here to get the git commit.
 
     """
-    return '0.0.35'
+    return '0.0.37'
